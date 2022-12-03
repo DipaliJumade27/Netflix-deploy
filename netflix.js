89c413ead1cff7f9942e35946204bf8c560d01e1
DipaliJumade27 <100439217+DipaliJumade27@users.noreply.github.com>
2022-12-03 14:06:07 +0530

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,60 @@
+// QUESTION 1
+const targetDiv = document.getElementById("answer1");
+const btn = document.getElementById("question1");
+btn.onclick = function () {
+	if(targetDiv.style.display !== "none"){
+		targetDiv.style.display = "none";
+	}else{
+		targetDiv.style.display = "block";
+	}
+};
+
+const Ans2 = document.getElementById("answer2");
+const question2btn = document.getElementById("question2");
+question2btn.onclick = function () {
+	if(Ans2.style.display !== "none"){
+		Ans2.style.display = "none";
+	}else{
+		Ans2.style.display = "block";
+	}
+};
+
+const Ans3 = document.getElementById("answer3");
+const question3btn = document.getElementById("question3");
+question3btn.onclick = function () {
+	if(Ans3.style.display !== "none"){
+		Ans3.style.display = "none";
+	}else{
+		Ans3.style.display = "block";
+	}
+};
+
+const Ans4 = document.getElementById("answer4");
+const question4btn = document.getElementById("question4");
+question4btn.onclick = function () {
+	if(Ans4.style.display !== "none"){
+		Ans4.style.display = "none";
+	}else{
+		Ans4.style.display = "block";
+	}
+};
+
+const Ans5 = document.getElementById("answer5");
+const question5btn = document.getElementById("question5");
+question5btn.onclick = function () {
+	if(Ans5.style.display !== "none"){
+		Ans5.style.display = "none";
+	}else{
+		Ans5.style.display = "block";
+	}
+};
+
+const Ans6 = document.getElementById("answer6");
+const question6btn = document.getElementById("question6");
+question6btn.onclick = function () {
+	if(Ans6.style.display !== "none"){
+		Ans6.style.display = "none";
+	}else{
+		Ans6.style.display = "block";
+	}
+};
